20ead0f9994c28bea783cc99ad6b40df8060bd8d
Pavel <Pavel.Vlasov@nasdanika.org>
2024-03-14 11:33:18 -0400

    2024.3.0

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -6,5 +6,4 @@ module org.nasdanika.models.enterprise {
 	requires transitive org.eclipse.emf.ecore;
 	requires transitive org.eclipse.emf.common;
 	requires transitive org.nasdanika.ncore;
-	requires transitive org.nasdanika.models.excel;
 }
